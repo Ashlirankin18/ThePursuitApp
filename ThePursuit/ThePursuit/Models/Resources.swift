3f9d06d93ac1c710107d2d8dbb97e934ea6e266a
Ashlirankin18 <ashlirankin18@gmail.com>
2019-03-27 18:39:49 -0400

    created the files for the class resources

--- ThePursuit/ThePursuit/Models/Resources.swift
+++ ThePursuit/ThePursuit/Models/Resources.swift
@@ -7,3 +7,18 @@
 //
 
 import Foundation
+
+struct Resources {
+  
+  let resourceName:String
+  let resourceLink:String
+  
+  init(resourceName:String,resourceLink:String){
+    self.resourceName = resourceName
+    self.resourceLink = resourceLink
+  }
+  init(dict:[String:Any]){
+    self.resourceLink = dict[ResourceCollectionKeys.resourceLink] as? String ?? "no resource link found"
+    self.resourceName = dict[ResourceCollectionKeys.resourceName] as? String ?? "no resource name found"
+  }
+}
