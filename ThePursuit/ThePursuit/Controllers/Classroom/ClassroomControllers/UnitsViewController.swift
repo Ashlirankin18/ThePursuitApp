8388eda01c6c94a3c306a1c6af6e43a1c1e9c6d1
natmess <nnmessier@gmail.com>
2019-03-27 21:49:35 -0400

    Set up table view Views for storyboard and in the process of populating my table views

--- ThePursuit/ThePursuit/Controllers/Classroom/ClassroomControllers/UnitsViewController.swift
+++ ThePursuit/ThePursuit/Controllers/Classroom/ClassroomControllers/UnitsViewController.swift
@@ -7,15 +7,76 @@
 //
 
 import UIKit
+import Firebase
 
 class UnitsViewController: UITableViewController {
-
-  let cellId = "UnitCell"
-  
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
+    
+    let cellId = "UnitCell"
+    let classData = [UnitsCollectionKeys.CollectionKey,ResourceCollectionKeys.CollectionKeys]
+    
+    private var listener: ListenerRegistration!
+    private var authservice = AppDelegate.authService
+    
+    private var units = [Units]() {
+        didSet {
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
     }
     
+//    @objc private func fetchBlogPosts() {
+//        refreshControl.beginRefreshing()
+//        listener = DBService.firestoreDB
+//            .collection(BlogsCollectionKeys.CollectionKey)
+//            .addSnapshotListener { [weak self] (snapshot, error) in
+//                if let error = error {
+//                    print("failed to fetch blogs with error: \(error.localizedDescription)")
+//                } else if let snapshot = snapshot {
+//                    self?.blogs = snapshot.documents.map { Blog(dict: $0.data()) }
+//                        .sorted { $0.createdDate.date() > $1.createdDate.date() }
+//                }
+//                DispatchQueue.main.async {
+//                    self?.refreshControl.endRefreshing()
+//                }
+//        }
+//    }
+//
+    @objc private func getUnits() {
+        listener = DBService.firestoreDB
+        .collection(UnitsCollectionKeys.CollectionKey)
+            .addSnapshotListener { [weak self] (snapshot, error) in
+                if let error = error {
+                    print("failed to fetch units with error: \(error.localizedDescription)")
+                } else if let snapshot = snapshot {
+                    self?.units = (snapshot.documents.map { Units(dict: $0.data()) })
+                }
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "UnitCell", for: indexPath)
+        cell.textLabel?.text = units.description
+        return cell
+    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return units.count
+    }
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        <#code#>
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        <#code#>
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        <#code#>
+    }
 
 }
--- ThePursuit/ThePursuit/Controllers/Main/PursuitTabViewController.swift
+++ ThePursuit/ThePursuit/Controllers/Main/PursuitTabViewController.swift
@@ -19,7 +19,9 @@ class PursuitTabViewController: UITabBarController {
 
   func setupViewControllers(){
     let generalController = UIStoryboard(name: "General", bundle: nil).instantiateViewController(withIdentifier: "GeneralViewController") as!  GeneralViewController
-    
+    let classroomViewController = UnitsViewController()
+    classroomViewController.tabBarItem = UITabBarItem(title: "Classroom", image: UIImage(named: "graduation_cap"), tag: 1)
+
     viewControllers = [generalController]
     
   }
