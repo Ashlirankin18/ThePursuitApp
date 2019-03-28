accd9a32a72cb68bb19c0cee1ff504398a8e0191
Jeffrey Almonte <jalmonte83@gmail.com>
2019-03-28 12:20:40 -0400

    preppin to merge nat's file into mine

--- ThePursuit/ThePursuit/Controllers/Main/PursuitTabViewController.swift
+++ ThePursuit/ThePursuit/Controllers/Main/PursuitTabViewController.swift
@@ -19,14 +19,14 @@ class PursuitTabViewController: UITabBarController {
 
   func setupViewControllers(){
     let generalController = UIStoryboard(name: "General", bundle: nil).instantiateViewController(withIdentifier: "GeneralViewController") as!  GeneralViewController
-<<<<<<< HEAD
+
     let profileController = UIStoryboard(name: "ProfileStoryboard", bundle: nil).instantiateViewController(withIdentifier: "ProfileViewController") as! ProfileViewController
-=======
+
     generalController.title = "General"
     generalController.tabBarItem.image = #imageLiteral(resourceName: "home.png")
->>>>>>> 84a64db31fa4fa17b2343e2b8b33a5ef407881a4
-    viewControllers = [generalController]
-    viewControllers = [profileController]
+
+    viewControllers = [generalController,profileController]
+
     
   }
 
