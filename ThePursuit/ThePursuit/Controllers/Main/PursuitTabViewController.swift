9da0fc41257e8c12667f75cea14b8d39b9bd8996
natmess <nnmessier@gmail.com>
2019-03-28 12:04:08 -0400

    updating for merge

--- ThePursuit/ThePursuit/Controllers/Main/PursuitTabViewController.swift
+++ ThePursuit/ThePursuit/Controllers/Main/PursuitTabViewController.swift
@@ -19,17 +19,15 @@ class PursuitTabViewController: UITabBarController {
 
   func setupViewControllers(){
     let generalController = UIStoryboard(name: "General", bundle: nil).instantiateViewController(withIdentifier: "GeneralViewController") as!  GeneralViewController
-<<<<<<< HEAD
+    
     let classroomViewController = UnitsViewController()
     classroomViewController.tabBarItem = UITabBarItem(title: "Classroom", image: UIImage(named: "graduation_cap"), tag: 1)
 
-    viewControllers = [generalController, classroomViewController]
-=======
+
     generalController.title = "General"
     generalController.tabBarItem.image = #imageLiteral(resourceName: "home.png")
-    viewControllers = [generalController]
->>>>>>> 84a64db31fa4fa17b2343e2b8b33a5ef407881a4
-    
+
+     viewControllers = [generalController, classroomViewController]
   }
 
 }
